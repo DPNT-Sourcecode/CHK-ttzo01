30edf56389e44d1fea6f7f4d7deb979562e75cde
sbx_user1051 <sbx_user1051@169.254.6.157>
2022-05-29 11:35:48 +0000

    Sun May 29 11:35:48 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,10 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+  if not isinstance(friend_name, str):
+    raise TypeError(f"Invalid input {friend_name}")
+
+  print("Hello world")
+  return "Hello World"  
+
