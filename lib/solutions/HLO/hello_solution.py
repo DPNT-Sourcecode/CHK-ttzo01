f4760050481e4218a415786fdd600804cef5cdd3
sbx_user1051 <sbx_user1051@169.254.6.157>
2022-05-29 11:36:43 +0000

    Sun May 29 11:36:43 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,5 +6,5 @@ def hello(friend_name: str) -> str:
   if not isinstance(friend_name, str):
     raise TypeError(f"Invalid input {friend_name}")
 
-  print("Hello world")
-  return "Hello World"  
+  return "Hello, World!"  
+
