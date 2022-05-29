d3f1c34b4cc9896008374096fdde42ab281d2085
sbx_user1051 <sbx_user1051@169.254.6.157>
2022-05-29 11:47:51 +0000

    Sun May 29 11:47:51 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,42 @@
 
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
+from typing import Dict
+
+PRICE_LIST = {
+  "A": 50,  
+  "B": 30,  
+  "C": 20,  
+  "D": 15,  
+}
+
+DEALS = {
+  "A": {
+    "count": 3,
+    "price": 130
+  },
+  "B": {
+    "count": 2,
+    "price": 45
+  }
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+  count_dict: Dict[str, int] = {}
+  for sku in skus:
+
+    if sku not in count:
+      count[sku] = 0
+    else:
+      count[sku] += 1
+    
+  
