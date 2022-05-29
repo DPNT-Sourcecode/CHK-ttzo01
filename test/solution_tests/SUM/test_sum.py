1a9e6504fe40b01f421d0f4caf35d248a5aa22ea
sbx_user1051 <sbx_user1051@169.254.6.157>
2022-05-29 11:30:15 +0000

    Sun May 29 11:30:15 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,13 @@
 from solutions.SUM import sum_solution
 
+from pytest import raises
 
 class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+  def test_sum(self):
+    assert sum_solution.compute(1, 2) == 3
+
+  def test_sum_out_of_range_raise_value_error(self):
+    with raises(ValueError):
+      sum_solution.compute(-1, 2)
+    with raises(ValueError):
+      sum_solution.compute(0, 101)
