192d2ee29ea0e2067bfcbb3f4f5f431c60f21805
sbx_user1051 <sbx_user1051@169.254.6.157>
2022-05-29 11:27:15 +0000

    Sun May 29 11:27:15 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+LOWER_BOUND_INCLUSIVE = 0
+UPPER_BOUND_EXCLUSIVE = 101
+
+SUM_RANGE = range(LOWER_BOUND_INCLUSIVE, UPPER_BOUND_EXCLUSIVE)
+
+def compute(x: int, y: int):
+  if x in SUM_RANGE and y in SUM_RANGE:
+    return x + y
+  else:
+    raise ValueError(f"Sum inputs not in range {SUM_RANGE}, x {x}, y {y}")
+
+
