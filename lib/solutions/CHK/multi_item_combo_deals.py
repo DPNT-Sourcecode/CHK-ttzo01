50a0c1f79b9108837cfb01eb70bf2779013ead8b
sbx_user1051 <sbx_user1051@169.254.238.45>
2022-05-29 18:31:00 +0000

    Sun May 29 18:31:00 UTC 2022

--- lib/solutions/CHK/multi_item_combo_deals.py
+++ lib/solutions/CHK/multi_item_combo_deals.py
@@ -47,7 +47,7 @@ def calculate_multi_item_combo_deal_price_reduction(count_dict: Dict[str, int], 
     remainder_after_deal_applied = total_count_for_combo % deal[DEAL_COUNT_KEY]
     deal_applied_count = floor(total_count_for_combo / deal[DEAL_COUNT_KEY])
 
-    sorted_ascending_prices_for_deal = dict(sorted(prices_for_deal.items(), key=lambda item: item[1]. reverse=True))
+    sorted_ascending_prices_for_deal = dict(sorted(prices_for_deal.items(), key=lambda item: item[1]))
 
     total_of_smallest_values_after_deal = 0
     for sku_and_price in sorted_ascending_prices_for_deal.items():
@@ -57,9 +57,6 @@ def calculate_multi_item_combo_deal_price_reduction(count_dict: Dict[str, int], 
         price = sku_and_price[1]
         count_for_sku = count_dict[sku]
 
-        if count_for_sku >= deal[DEAL_COUNT_KEY]:
-          count_for_sku = count_for_sku % deal[DEAL_COUNT_KEY]
-
         if remainder_after_deal_applied <= count_for_sku:
           total_of_smallest_values_after_deal += remainder_after_deal_applied * price
           break
@@ -68,15 +65,11 @@ def calculate_multi_item_combo_deal_price_reduction(count_dict: Dict[str, int], 
           total_of_smallest_values_after_deal += count_for_sku * price
           remainder_after_deal_applied -= count_for_sku
 
-    print("total_of_smallest_values_after_deal", total_of_smallest_values_after_deal)
-    
     expected_cost_of_deal = total_of_smallest_values_after_deal + deal_applied_count * deal[TOTAL_PRICE_KEY]
-    print(expected_cost_of_deal)
     total_price_reduction = cost_without_deal - expected_cost_of_deal
 
-    print(total_price_reduction)
-
   return total_price_reduction
+
 
 
 
