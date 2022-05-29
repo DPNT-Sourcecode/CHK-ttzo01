6cada1b723b21c8ae13751e311c43fc71f69f4d2
sbx_user1051 <sbx_user1051@169.254.238.45>
2022-05-29 17:37:00 +0000

    Sun May 29 17:37:00 UTC 2022

--- lib/solutions/CHK/multi_item_combo_deals.py
+++ lib/solutions/CHK/multi_item_combo_deals.py
@@ -6,6 +6,8 @@ from solutions.CHK.data_keys import (
   PRICE_REDUCTION_KEY,
   MULTI_ITEM_DEAL_KEY
 )
+from solutions.CHK.price_list import PRICE_LIST
+
 
 # | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
 # | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
@@ -26,11 +28,20 @@ MULTI_ITEM_COMBO_DEALS = [
   },
 ]
 
-def calculate_multi_item_deal_price_reduction(count_dict: Dict[str, int], unique_skus: list) -> int:
+def calculate_multi_item_combo_deal_price_reduction(count_dict: Dict[str, int], unique_skus: list) -> int:
   total_price_reduction = 0
 
   total_count_for_combo = 0
+
+  cost_without_deal = 0
+  for unique_sku in unique_skus:
+
   for deal in DEAL_COMBO_LIST_KEY:
-    for sku_in_combo_deal in deal[DEAL_COMBO_LIST_KEY]
+    for sku_in_combo_deal in deal[DEAL_COMBO_LIST_KEY]:
+      if sku_in_combo_deal in count_dict:
+        total_count_for_combo += count_dict[sku_in_combo_deal]
+        cost_without_deal += count_dict[sku_in_combo_deal] * PRICE_LIST[sku_in_combo_deal]
+        
+
 
   return total_price_reduction
